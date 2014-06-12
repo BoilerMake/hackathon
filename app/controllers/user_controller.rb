7c9d7613009e2cbd48c12f202674a18f10d62ffd
Scott Opell <me@scottopell.com>
2014-06-11 23:33:31 -0400

    initial route and controller for signup

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,10 @@
 class UserController < ApplicationController
+  def new
+    @greeting_message = t :new_user_signup
+  end
+
+  def sign_up
+    @greeting_message = t :new_hacker_signup
+    render action: :new
+  end
 end
