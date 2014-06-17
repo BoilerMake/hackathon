fd2bec2135b5f92cc5dc03c4bfd1fbbfbb1ceb67
Scott Opell <me@scottopell.com>
2014-06-17 03:39:55 -0400

    adds remember me to auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ActiveRecord::Base
   validates_presence_of :first_name
   validates_presence_of :last_name
   validates_presence_of :email
+  before_create { generate_token(:auth_token) }
+
+  def generate_token(column)
+    begin
+      self[column] = SecureRandom.urlsafe_base64
+    end while User.exists?(column => self[column])
+  end
 end
