164a5afedd7d33b6379cab5bf45e9cbd866e3907
Kirby Kohlmorgen <kirby@purdue.edu>
2014-06-19 23:38:10 -0700

    Add regex to validate .edu on email

--- app/models/hacker.rb
+++ app/models/hacker.rb
@@ -3,6 +3,8 @@ class Hacker < User
   belongs_to :team
   has_one :application
 
+  validates_format_of :email, with: /\A.*?\.edu\z/i
+
   def accepted?
     false
   end
--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,10 @@ ActiveRecord::Schema.define(version: 20140620032535) do
   end
 
   create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "school_id"
+    t.integer  "team_id"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -45,10 +49,6 @@ ActiveRecord::Schema.define(version: 20140620032535) do
     t.string   "password_reset_token"
     t.datetime "password_reset_sent_at"
     t.string   "type"
-    t.string   "last_name"
-    t.string   "first_name"
-    t.integer  "team_id"
-    t.integer  "school_id"
   end
 
 end
