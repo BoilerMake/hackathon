293f055e6c9e0e89f208c9e724cf61ea9f68c0ce
Spencer Brown <me@spencer.sx>
2014-06-21 14:17:36 -0400

    ensure uniquness of secret_key

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,6 +3,8 @@ class Team < ActiveRecord::Base
   before_create { generate_secret_key }
 
   def generate_secret_key
-    self.secret_key = SecureRandom.urlsafe_base64
+    begin
+      self.secret_key = SecureRandom.urlsafe_base64
+    end while Team.exists?(secret_key: self.secret_key)
   end
 end
