3561d20c9ce99eb92919cbbdbdb707011059cd1a
Spencer Brown <me@spencer.sx>
2014-06-22 00:44:39 -0400

    don't validate presence of application fields

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,10 +1,13 @@
 class Application < ActiveRecord::Base
-  validates_presence_of :gender
-  validates_presence_of :essay
-  validates_presence_of :gender
-  validates_presence_of :github
-  validates_presence_of :previous_experience
-  validates_presence_of :essay
+  # I don't think we need to do this. We can accept incomplete applications, right?
+  # This has the benefit of letting people complete their applications over time.
+  # -Spencer
+  # validates_presence_of :gender
+  # validates_presence_of :essay
+  # validates_presence_of :gender
+  # validates_presence_of :github
+  # validates_presence_of :previous_experience
+  # validates_presence_of :essay
 
   belongs_to :hacker, foreign_key: 'hacker_id'
 
