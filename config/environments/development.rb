e7629c8bca62ea263e1143dcac8d191319f09b21
Kirby Kohlmorgen <kirby@purdue.edu>
2014-06-22 16:26:33 -0700

    Moved mailer logic to enviroment files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,16 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  ActionMailer::Base.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "gmail.com",
+    :user_name            => "purduehonors26@gmail.com",
+    :password             => "neilcroy",
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+
+  ActionMailer::Base.default_url_options[:host] = "localhost:3000"
 end
