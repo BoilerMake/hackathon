ef18c060b5cb446fec82a82fec057ce93c580dc6
Scott Opell <me@scottopell.com>
2014-06-22 19:51:00 -0400

    refactored team invite and added validation for 4 person teams

--- app/mailers/team_mailer.rb
+++ app/mailers/team_mailer.rb
@@ -1,5 +1,6 @@
 class TeamMailer < ActionMailer::Base
-  default from: "purduehonors26@gmail.com"
+  add_template_helper(TeamHelper)
+  default from: DEFAULT_EMAIL
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -9,6 +10,6 @@ class TeamMailer < ActionMailer::Base
   def invite_hacker(email, team, current_user)
     @team = team
     name = current_user.first_name ? current_user.first_name : 'A friend'
-    mail(to: email, subject: "#{name} wants you to join a BoilerMake team!")
+    mail(to: email, subject: "#{name} wants you to join a #{HACKATHON} team!")
   end
 end
