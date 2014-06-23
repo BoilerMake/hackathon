d88f13d607842f47c94da5a7d79e9a47707d6688
Kirby Kohlmorgen <kirby@purdue.edu>
2014-06-22 22:35:10 -0700

    Schools complete

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,9 +1,9 @@
 $(function() {
+    console.log(document.cookie);
   $.get('/schools.json', function(data) {
     schools = _.map(data, function(school) {
       return school['name'];
     })
-
     $("#schools").autocomplete({
       source: schools
     });
