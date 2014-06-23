7c549b6324909721cd7075c8643f8fa3ea025154
Kirby Kohlmorgen <kirby@purdue.edu>
2014-06-23 01:29:21 -0700

    Add bootstrap back in

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,1 @@
-Rails.application.config.assets.precompile += %w(welcome.css hackers.css teams.css)
+Rails.application.config.assets.precompile += %w(bootstrap.css welcome.css hackers.css teams.css)
