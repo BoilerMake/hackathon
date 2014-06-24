b3402637623cc4cf2a973944ce07763f7db47da8
Kirby Kohlmorgen <kirby@purdue.edu>
2014-06-23 23:54:42 -0700

    js for selecting team link

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -2,4 +2,13 @@ $(function() {
   $('#invite-send').on('click', function() {
     $(this).parents('form:first').submit();
   })
+  $('#invitelink input').focus(function() {
+    var $this = $(this);
+    $this.select();
+
+    $this.mouseup(function() {
+      $this.unbind("mouseup");
+      return false;
+    });
+  });
 });
