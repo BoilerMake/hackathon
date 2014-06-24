2192ac8de260c3c56705c601eb11248e0e012671
Kirby Kohlmorgen <kirby@purdue.edu>
2014-06-23 21:19:11 -0700

    Fix invite bug, where to: field wasn't getting filled

--- app/controllers/hacker_invitations_controller.rb
+++ app/controllers/hacker_invitations_controller.rb
@@ -8,10 +8,10 @@ class HackerInvitationsController < ApplicationController
     invitees = [params[:email_one], params[:email_two], params[:email_three]]
 
     invitees.each do |invitee|
-      team.send_hacker_invitation(invitee, current_user) unless invitee.nil?
+      team.send_hacker_invitation(invitee, current_user) if invitee.present?
     end
 
-    flash[:notice] = 'Invitations sent.'
+    flash[:notice] = 'Invitation(s) sent.'
     redirect_to my_team_path
   end
 end
