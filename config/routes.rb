ba9c70e3b963cf283f1c10ec93cfe04562e7df4a
Scott Opell <me@scottopell.com>
2014-07-17 01:46:14 -0400

    cleans up routes a little bit

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
 
   resources :schools, only: [:index, :show]
-  resources :hackers, except: [:index]
+  resources :hackers, except: [:index, :destroy]
   resources :teams, except: [:show, :index, :destroy]  do
     get 'remove_hacker', on: :member
   end
