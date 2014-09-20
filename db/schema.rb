c948f7e62f199b2e810b4321940450442fe7b63b
Scott Opell <me@scottopell.com>
2014-09-20 13:49:24 -0400

    adds status to hackers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140622203529) do
+ActiveRecord::Schema.define(version: 20140920163511) do
 
   create_table "applications", force: true do |t|
     t.string   "gender"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20140622203529) do
     t.string   "password_reset_token"
     t.datetime "password_reset_sent_at"
     t.string   "type"
+    t.string   "status"
   end
 
 end
