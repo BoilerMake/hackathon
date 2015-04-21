4c8ed10c7ded96ca28b603dd653eccf973943529
Shane DeWael <dewaels@purdue.edu>
2015-04-21 18:15:19 -0400

    After logo + front end

--- app/assets/javascripts/enigma.js
+++ app/assets/javascripts/enigma.js
@@ -5,6 +5,8 @@ Enigma = {
   colors: ['rgba(230, 73, 73,', 'rgba(199, 181, 88,', 'rgba(143, 112, 97,'],
   validChars: "0123456789ABCDEFGHIJKLMNOPQURSTUVWXYZ",
   interval: 0,
+  message: "BoilerMake",
+  messageCounter: 0,
   charPressed: '',
   canvas: 0,
   ctx: 0,
@@ -22,9 +24,9 @@ Enigma = {
     Enigma.canvas.height = s.height;
     Enigma.canvas.width = s.width;
     Enigma.ctx = Enigma.canvas.getContext("2d");
-    for (var i = 0; i < s.height/s.size; i++) {
+    for (var i = 0; i < s.height/s.size * .35; i++) {
       Enigma.keys[i] = [];
-      for (var j = 0; j < s.width/s.size; j++) {
+      for (var j = 0; j < s.width/s.size * .25; j++) {
         Enigma.keys[i][j] = new Enigma.key(i, j);
       }
     }
@@ -33,6 +35,7 @@ Enigma = {
   key: function(i, j) {
     this.character = Enigma.validChars.substr( Math.floor(Math.random() * 36), 1);
     this.light = 0;
+    this.messageC = 0;
     this.x =  (s.size * 1.3) + (4 * j * s.size);
     this.y = -(s.size * .3) + (s.size * 3 * i);
     this.color = Enigma.colors[i % 3];
@@ -64,7 +67,11 @@ Enigma = {
         this.character = Enigma.charPressed;
       }
       if (this.light > 0) this.light--;
-      Enigma.ctx.fillText(this.character, this.x+1, this.y+(s.size/2.5)-1);
+      if (this.messageC != 0) {
+        Enigma.ctx.fillText(this.messageC, this.x+1, this.y+(s.size/2.5)-1);
+      } else {
+        Enigma.ctx.fillText(this.character, this.x+1, this.y+(s.size/2.5)-1);
+      }
     };
   },
   lightKey: function(p, delay, duration) {
@@ -72,10 +79,20 @@ Enigma = {
       p.light = duration;
     }, Math.random() * delay);
   },
+  messageKey: function(p, delay, duration, j) {
+    window.setTimeout(function(){ 
+      for (var i = (Math.round(s.width/s.size * .25 * .5) - (Enigma.message.length * .5)); i <= (Math.round(s.width/s.size * .25 * .5) + (Enigma.message.length * .5)); i++) {
+        if (j==i) {
+          p.messageC = Enigma.message.substr((j-(Math.round(s.width/s.size * .25 * .5) - (Enigma.message.length * .5)))-1, 1);
+        }
+      }
+      p.color = Enigma.colors[1];
+      p.light = duration;
+    }, Math.random() * delay);
+  },
   userActions: function() {
     window.addEventListener('resize', function() {
-      //Enigma.stopLoop();
-      console.log("Resized window");
+      //TODO: fix resize bug
       s.amount = Math.round(window.innerWidth);
       s.width = window.innerWidth;
       s.height = window.innerHeight;
@@ -83,9 +100,9 @@ Enigma = {
       Enigma.canvas.height = s.height;
       Enigma.canvas.width = s.width;
       Enigma.ctx = Enigma.canvas.getContext("2d");
-      for (var i = 0; i < s.height/s.size; i++) {
+      for (var i = 0; i < s.height/s.size * .35; i++) {
         Enigma.keys[i] = [];
-        for (var j = 0; j < s.width/s.size; j++) {
+        for (var j = 0; j < s.width/s.size * .25; j++) {
           Enigma.keys[i][j] = new Enigma.key(i, j);
         }
       }
@@ -94,16 +111,24 @@ Enigma = {
     $('#email').keypress(function(e) {
       if (e.which === 13) {
         $('#logo').css('display', 'none');
-         for (var i = 0; i < s.height/s.size; i++) {
-          for (var j = 0; j < s.width/s.size; j++) {
+         for (var i = 0; i < s.height/s.size * .35; i++) {
+          for (var j = 0; j < s.width/s.size * .25; j++) {
             Enigma.charPressed = ' ';
-            Enigma.lightKey(Enigma.keys[i][j], 4000, -1);
+            if (i == Math.round((s.height/s.size*.35) * .5) ) {
+              if ((j > Math.round(s.width/s.size * .25 * .5) - (Enigma.message.length * .5)) && (j < Math.round(s.width/s.size * .25) * .5 + (Enigma.message.length * .5))) {
+                Enigma.messageKey(Enigma.keys[i][j], 250, 10, j);
+              } else {
+                Enigma.lightKey(Enigma.keys[i][j], 4000, -1);
+              }
+            } else {
+              Enigma.lightKey(Enigma.keys[i][j], 4000, -1);
+            }
           }
         }
         } else {
           Enigma.charPressed = String.fromCharCode(e.which);
-          for (var i = 0; i < s.height/s.size; i++) {
-            for (var j = 0; j < s.width/s.size; j++) {
+          for (var i = 0; i < s.height/s.size*.35; i++) {
+            for (var j = 0; j < s.width/s.size*.25; j++) {
               if (Math.random() < 0.1) {
                 Enigma.lightKey(Enigma.keys[i][j], 250, 10);
               } else {
@@ -117,8 +142,8 @@ Enigma = {
   startLoop: function() {
     interval = window.setInterval(function() {
       Enigma.ctx.clearRect(0, 0, Enigma.canvas.width, Enigma.canvas.height);
-      for (var i = 0; i < s.height/s.size; i++) {
-        for (var j = 0; j < s.width/s.size; j++) {
+      for (var i = 0; i < s.height/s.size*.35; i++) {
+        for (var j = 0; j < s.width/s.size*.25; j++) {
           Enigma.keys[i][j].draw();
         }
       }
