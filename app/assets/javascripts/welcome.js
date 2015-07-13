dab8cb1772e320c52a3531cc288f552c308ebf44
Benjamin Alderfer <Ben@Benjamins-MacBook-Pro-2.local>
2015-07-12 22:01:02 -0700

    Show/hide fields

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,5 +1,12 @@
 /*global $:false */
 
+var showForm = function(id) {
+  $(id).css('display', 'block');
+};
+var hideForm = function(id) {
+  $('.full-form').css('display', 'none');
+};
+
 $(document).ready(function(){
   $("#new_interest_signup").on('ajax:success', function(e, data, status, xhr){
     console.log(data);
