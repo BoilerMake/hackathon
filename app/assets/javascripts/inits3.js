82831730d979f149c58ce436d69d66d932071b3a
Kedar <kedar54@gmail.com>
2015-07-13 14:51:49 -0500

    upload populates input with name 'resume'
    success message needs cleaning up

--- app/assets/javascripts/inits3.js
+++ app/assets/javascripts/inits3.js
@@ -1,7 +1,15 @@
 $(function() {
 	$('.js-s3_file_field').S3FileField({
       done: function(e, data) {
-        return console.log(data.result.url);
+      	$("#hidden_resume").val(data.result.url);
+      	$("#info").addClass("alert alert-success").fadeIn();
+      	$(".js-s3_file_field").fadeOut();
+        // return console.log(data.result.url);
       }
      });
+	$("#refresh").click(function(e) {
+		$(".js-s3_file_field").fadeIn();
+		$("#info").addClass("alert alert-success").fadeOut();
+		e.preventDefault();
+	});
 });
