af564a837e031f17098be92a4fd96eb4242156de
Kedar <kedar54@gmail.com>
2015-07-13 15:32:28 -0500

    cleaned up success alert

--- app/assets/javascripts/inits3.js
+++ app/assets/javascripts/inits3.js
@@ -2,14 +2,14 @@ $(function() {
 	$('.js-s3_file_field').S3FileField({
       done: function(e, data) {
       	$("#hidden_resume").val(data.result.url);
-      	$("#info").addClass("alert alert-success").fadeIn();
-      	$(".js-s3_file_field").fadeOut();
+      	$("#info").addClass("alert alert-success file_alert").fadeIn();
+      	$(".js-s3_file_field").hide();
         // return console.log(data.result.url);
       }
      });
 	$("#refresh").click(function(e) {
 		$(".js-s3_file_field").fadeIn();
-		$("#info").addClass("alert alert-success").fadeOut();
+		$("#info").addClass("alert alert-success file_alert").hide();
 		e.preventDefault();
 	});
 });
