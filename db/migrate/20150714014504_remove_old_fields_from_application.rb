4a25f47dbba6e1daddfd2afe99a495fd1aa16b71
Nicky Semenza <14nicholasse@gmail.com>
2015-07-13 19:08:50 -0700

    tab fix

--- db/migrate/20150714014504_remove_old_fields_from_application.rb
+++ db/migrate/20150714014504_remove_old_fields_from_application.rb
@@ -4,11 +4,11 @@ class RemoveOldFieldsFromApplication < ActiveRecord::Migration
     remove_column :applications, :previous_experience
     remove_column :applications, :expected_graduation
     remove_column :applications, :address_line_one
-	remove_column :applications, :address_line_two
-	remove_column :applications, :city
-	remove_column :applications, :state
-	remove_column :applications, :zip_code
-	remove_column :applications, :badge_id
-	remove_column :applications, :essay
+    remove_column :applications, :address_line_two
+    remove_column :applications, :city
+    remove_column :applications, :state
+    remove_column :applications, :zip_code
+    remove_column :applications, :badge_id
+    remove_column :applications, :essay
   end
 end
