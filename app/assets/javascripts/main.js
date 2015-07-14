83bb1f84cfaf26be6262951d37a1275487cf005f
Kedar <kedar54@gmail.com>
2015-07-13 20:35:31 -0500

    began signup

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,21 @@
+$(function() {
+  $('form.new_hacker').submit(function(event) {
+    var form = $(this);
+    $.ajax({
+      type: form.attr('method'),
+      url: form.attr('action'),
+      data: form.serialize(),
+      success: function (data) {
+        var string = "";
+        jQuery.each(data, function(i, val) {
+          if(i == "success") {
+            alert("SUCCESS YO");
+          }
+          string += (i + " " + val);
+        });
+        $("#signup_alert").addClass("alert alert-danger").html(string + "<br/>").fadeIn();
+      }
+    });
+    event.preventDefault(); // Prevent the form from submitting via the browser.
+  });
+});
