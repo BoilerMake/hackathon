0de56b552c4286275bf12ffb59ddb6edb53eb57a
Ben Alderfer <benalderfer@mbp-balderfer.local>
2015-07-14 12:41:05 -0700

    Disable submit while uploading resume

--- app/assets/javascripts/inits3.js
+++ app/assets/javascripts/inits3.js
@@ -4,7 +4,12 @@ $(function() {
       	$("#hidden_resume").val(data.result.url);
       	$("#info").addClass("alert alert-success file_alert").fadeIn();
       	$(".js-s3_file_field").hide();
+        $('#save').attr('disabled', 'false');
         // return console.log(data.result.url);
+      },
+      add: function(e, data) {
+        $('#save').attr('disabled', 'true');
+        data.submit();
       }
      });
 	$("#refresh").click(function(e) {
