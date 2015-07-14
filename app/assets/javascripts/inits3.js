fc43d9cba4a9737f65d635c3ff623cf97cff8bdc
Ben Alderfer <benalderfer@mbp-balderfer.local>
2015-07-14 14:28:25 -0700

    Use prop instead of attr

--- app/assets/javascripts/inits3.js
+++ app/assets/javascripts/inits3.js
@@ -3,12 +3,12 @@ $(function() {
       done: function(e, data) {
         $("#hidden_resume").val(data.result.url);
         $("#info span.file-text").html("Upload Complete!");
-        $('#save').attr('disabled', 'false');
+        $('#save').prop('disabled', false);
         $("#info").removeClass("alert-info").addClass("alert-success");
         $("#info span.new-file-prompt a").html("Upload new file");
       },
       add: function(e, data) {
-        $('#save').attr('disabled', 'true');
+        $('#save').prop('disabled', true);
         $(".js-s3_file_field").hide();
         $("#info").addClass("alert alert-info file_alert").fadeIn();
         $("#info span.new-file-prompt a").html("");
