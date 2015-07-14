d1844c1a0bca12643e0f0d6dd61b52ba157a6ea4
Scott Opell <me@scottopell.com>
2015-07-14 16:34:03 -0400

    initial stab at disabling form and displaying correct status on uploaded file

--- app/assets/javascripts/inits3.js
+++ app/assets/javascripts/inits3.js
@@ -1,20 +1,24 @@
 $(function() {
-	$('.js-s3_file_field').S3FileField({
+  $('.js-s3_file_field').S3FileField({
       done: function(e, data) {
-      	$("#hidden_resume").val(data.result.url);
-      	$("#info").addClass("alert alert-success file_alert").fadeIn();
-      	$(".js-s3_file_field").hide();
+        $("#hidden_resume").val(data.result.url);
+        $("#info").addClass("alert alert-success file_alert").fadeIn();
+        $(".js-s3_file_field").hide();
         $('#save').attr('disabled', 'false');
-        // return console.log(data.result.url);
       },
       add: function(e, data) {
         $('#save').attr('disabled', 'true');
         data.submit();
+      },
+      progress: function(e, data){
+        $('form :submit').attr("disabled", "disabled");
+        progress = parseInt(data.loaded / data.total * 100, 10);
+        $("#info span").html("File " + progress + "% uploaded");
       }
      });
-	$("#refresh").click(function(e) {
-		$(".js-s3_file_field").fadeIn();
-		$("#info").addClass("alert alert-success file_alert").hide();
-		e.preventDefault();
-	});
+  $("#refresh").click(function(e) {
+    $(".js-s3_file_field").fadeIn();
+    $("#info").addClass("alert alert-success file_alert").hide();
+    e.preventDefault();
+  });
 });
