4e4e6c9b3792aba4b2ebd268834f97ffabe0eadc
Scott Opell <me@scottopell.com>
2015-07-14 17:02:45 -0400

    minor ux changes with behavior of file upload

--- app/assets/javascripts/inits3.js
+++ app/assets/javascripts/inits3.js
@@ -2,19 +2,21 @@ $(function() {
   $('.js-s3_file_field').S3FileField({
       done: function(e, data) {
         $("#hidden_resume").val(data.result.url);
-        $("#info span").html("Upload Complete!");
+        $("#info span.file-text").html("Upload Complete!");
         $('#save').attr('disabled', 'false');
         $("#info").removeClass("alert-info").addClass("alert-success");
+        $("#info span.new-file-prompt a").html("Upload new file");
       },
       add: function(e, data) {
         $('#save').attr('disabled', 'true');
         $(".js-s3_file_field").hide();
         $("#info").addClass("alert alert-info file_alert").fadeIn();
+        $("#info span.new-file-prompt a").html("");
         data.submit();
       },
       progress: function(e, data){
         progress = parseInt(data.loaded / data.total * 100, 10);
-        $("#info span").html("File " + progress + "% uploaded");
+        $("#info span.file-text").html("File " + progress + "% uploaded");
       }
      });
   $("#refresh").click(function(e) {
