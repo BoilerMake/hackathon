7610e41e737aee97f8911a2509076199237a1d4f
Scott Opell <me@scottopell.com>
2015-07-14 16:42:17 -0400

    polish on the progress thing

--- app/assets/javascripts/inits3.js
+++ app/assets/javascripts/inits3.js
@@ -2,16 +2,17 @@ $(function() {
   $('.js-s3_file_field').S3FileField({
       done: function(e, data) {
         $("#hidden_resume").val(data.result.url);
-        $("#info").addClass("alert alert-success file_alert").fadeIn();
-        $(".js-s3_file_field").hide();
+        $("#info span").html("Upload Complete!");
         $('#save').attr('disabled', 'false');
+        $("#info").removeClass("alert-info").addClass("alert-success");
       },
       add: function(e, data) {
         $('#save').attr('disabled', 'true');
+        $(".js-s3_file_field").hide();
+        $("#info").addClass("alert alert-info file_alert").fadeIn();
         data.submit();
       },
       progress: function(e, data){
-        $('form :submit').attr("disabled", "disabled");
         progress = parseInt(data.loaded / data.total * 100, 10);
         $("#info span").html("File " + progress + "% uploaded");
       }
