73ae08d0169dbdf3fd1914e19efd9b90aab92655
Nicky Semenza <14nicholasse@gmail.com>
2015-07-13 18:58:30 -0700

    removed old DB columns from applications table

---
+++ db/migrate/20150714014504_remove_old_fields_from_application.rb
@@ -0,0 +1,14 @@
+class RemoveOldFieldsFromApplication < ActiveRecord::Migration
+  def change
+    remove_column :applications, :linkedin
+    remove_column :applications, :previous_experience
+    remove_column :applications, :expected_graduation
+    remove_column :applications, :address_line_one
+	remove_column :applications, :address_line_two
+	remove_column :applications, :city
+	remove_column :applications, :state
+	remove_column :applications, :zip_code
+	remove_column :applications, :badge_id
+	remove_column :applications, :essay
+  end
+end
