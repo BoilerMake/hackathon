b3072a1d46fc6a2e742121c432ddacf91f3c4c21
Nicky Semenza <14nicholasse@gmail.com>
2015-07-14 17:56:26 -0700

    hide second job question on fresh form load

--- app/assets/javascripts/hackers.js
+++ app/assets/javascripts/hackers.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
+    //for loading values from db
     var loaded_option = $('#hacker_application_attributes_job_interest').val();
-    if (loaded_option == 'not interested') {
+    if (loaded_option == 'not interested' || loaded_option == '') {
         $(".job-dates").hide();
     } else {
         $(".job-dates").show();
     }
-
+    //for changing
     $('#hacker_application_attributes_job_interest').on('change', function() {
       if ( this.value == 'not interested') {
         $(".job-dates").hide();
