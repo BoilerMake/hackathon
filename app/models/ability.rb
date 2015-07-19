9cbb8b135ac4f66a712aa327bb03b109edf52ae9
Scott Opell <me@scottopell.com>
2015-07-19 16:40:49 -0700

    hackers need index access to school index

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,6 +9,7 @@ class Ability
       elsif user.hacker?
         can :manage, [Application, Team]
         can [ :dashboard, :new, :create, :update, :confirm ], Hacker
+        can [ :index ], School
       end
     end
     can :index, Update
