adb4128886c09d9b1c34e5ce47719e8fbb8ff135
Kedar <kedar54@gmail.com>
2015-07-20 10:16:35 -0500

    random changes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,1 @@
-Rails.application.config.assets.precompile += %w(bootstrap.min.css pages.css schools.css hackers.css teams.css header.css updates.css)
+Rails.application.config.assets.precompile += %w(bootstrap.min.js bootstrap.min.css pages.css hackers.css teams.css header.css updates.css)
