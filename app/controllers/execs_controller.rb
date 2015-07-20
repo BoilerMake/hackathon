8ef9ae51a8c240a1dfca0a5042aa95acf5688385
Nicky Semenza <14nicholasse@gmail.com>
2015-07-20 11:05:51 -0700

    github credentials come from env vars now

--- app/controllers/execs_controller.rb
+++ app/controllers/execs_controller.rb
@@ -117,14 +117,14 @@ class ExecsController < ApplicationController
     http = Net::HTTP.new(parsed_url.host, parsed_url.port)
     http.use_ssl = true
     request = Net::HTTP::Get.new(parsed_url.request_uri)
-    request.basic_auth 'boilermake-web', 'cff6c84e6672405c31e9e36166bbaa3011d21f4b'
+    request.basic_auth ENV['github_api_username'], ENV['github_api_secret']
     @hacker_github= ActiveSupport::JSON.decode(http.request(request).body)
 
     parsed_url = URI.parse("https://api.github.com/users/#{github_username}/repos?sort=updated")
     http = Net::HTTP.new(parsed_url.host, parsed_url.port)
     http.use_ssl = true
     request = Net::HTTP::Get.new(parsed_url.request_uri)
-    request.basic_auth 'boilermake-web', 'cff6c84e6672405c31e9e36166bbaa3011d21f4b'
+    request.basic_auth ENV['github_api_username'], ENV['github_api_secret']
     @hacker_github_repos= ActiveSupport::JSON.decode(http.request(request).body)
 
     query = HackerRanking.where("hacker_id = ? and exec_id = ?", @hacker.id, current_user.id)
