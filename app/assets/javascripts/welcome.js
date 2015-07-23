38706d1f6bafa170459af7233376b5b003e03942
Shane DeWael <dewaels@purdue.edu>
2015-07-22 21:49:03 -0400

    Navigation

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -13,4 +13,24 @@ $(document).ready(function(){
   }).on('ajax:error', function(e, data, status, error){
     console.log(error);
   });
+  var origTop = null;
+  $(window).on("scroll", function(e) {
+  	var scrollTop, navTop, isFixed;
+  	var nav = $('.navigation');
+  	scrollTop = $(window).scrollTop();
+  	navTop = nav.offset().top;
+  	isFixed = nav.hasClass('fix-div');
+  	if (origTop === null) {
+  		origTop = navTop;
+  	}
+  	if (isFixed && scrollTop <= origTop) {
+  		nav.removeClass('fix-div');
+  		$('.replace-div').css("display", "none");
+  	} else if (scrollTop >= navTop && !isFixed) {
+  		nav.addClass('fix-div');
+  		$('.replace-div').css("display", "block");
+  	}
+  });
 });
+
+
