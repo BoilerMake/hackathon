7a14b068fec94417a3c67a46f0425bb9baf1a223
Scott Opell <me@scottopell.com>
2015-10-05 13:37:01 -0400

    adds hack for sponsor password_resets

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -9,7 +9,7 @@ class PasswordResetsController < ApplicationController
 
   def update
     @user = User.find_by_password_reset_token!(params[:id])
-    if @user.password_reset_sent_at < 2.hours.ago
+    if !@user.sponsor? && @user.password_reset_sent_at < 2.hours.ago
       redirect_to root_url, alert: "Password reset has expired."
     elsif @user.update_attributes user_params
       redirect_to root_url, notice: "Password has been reset."
