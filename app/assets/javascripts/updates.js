442a0f5685db8709d24b963f32c5b297781200af
Scott Opell <me@scottopell.com>
2015-10-14 18:46:24 -0400

    adds in JS for alt txt version of updates

---
+++ app/assets/javascripts/updates.js
@@ -0,0 +1,8 @@
+$(function(){
+  $("#use_short").on( 'change', function(){
+      $("#short_msg").toggle();
+    });
+  $("#update_should_text").on('change', function(){
+      $(".text-options").toggle();
+    });
+});
