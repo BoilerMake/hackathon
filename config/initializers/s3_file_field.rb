972ea172a9b70d2cd6774196d84f4467f5e0a983
Kedar <kedarv@users.noreply.github.com>
2018-02-11 13:59:27 -0600

    Update s3_file_field.rb

--- config/initializers/s3_file_field.rb
+++ config/initializers/s3_file_field.rb
@@ -1,6 +1,6 @@
 S3FileField.config do |c|
-  c.access_key_id = "AKIAI5VU4VHOMIEYXJGA"
-  c.secret_access_key = "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"
+  c.access_key_id = ""
+  c.secret_access_key = ""
   c.bucket = "boilermake-resumes"
   c.key_starts_with = 'resume/'
   c.max_file_size = 25.megabytes
